7713138b837fa708feeba974caae9b319ad132d4
rattenreiter <martin.meier@hu-berlin.de>
2022-08-03 15:49:39 +0200

    Finished Template and Post Objects

--- app.js
+++ app.js
@@ -16,12 +16,7 @@ app.use(bodyParser.urlencoded({ extended:true }));
 let ejsObjects = {
     siteTitle: '',
     siteText: '',
-    posts: [
-        {
-            postHeadline: '',
-            postText: ''
-        }
-    ]
+    posts: []
 };
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -32,9 +27,8 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 app.get("/", (req, res) => {
     ejsObjects.siteTitle = 'Home';
     ejsObjects.siteText = homeStartingContent;
-    console.log(ejsObjects.posts);
     res.render("site", ejsObjects);
-
+    console.log(ejsObjects);
 });
 
 app.get("/about", (req, res) => {
@@ -57,16 +51,24 @@ app.get("/compose", (req, res) => {
     res.render("site", ejsObjects);
 });
 
+// Dynamic Routes
+// ---------------------------------------------
+
 // POSTS
 // ---------------------------------------------
 app.post("/", (req, res) => {
     let postHeadline = req.body.postHeadline;
     let postText = req.body.postText;
+    let postLink = postHeadline.replace(/[^0-9a-z\ ]/gi, '').toLocaleLowerCase();
+    let postExcerpt = postText.slice(0, 100);
     let postObject = {
         postHeadline: postHeadline,
-        postText: postText
+        postText: postText,
+        postExcerpt: postExcerpt,
+        postLink: '/' + postLink.replace(/ /g,'-')
     };
     ejsObjects.posts.push(postObject);
+
     res.redirect("/");
 });
 
