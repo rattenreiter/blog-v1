499b70febb6671a61c0f93a99b34292ad967809a
rattenreiter <martin.meier@hu-berlin.de>
2022-08-04 16:03:14 +0200

    Partially working Mongoose Implementation

--- app.js
+++ app.js
@@ -3,53 +3,119 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
+const mongoose = require('mongoose');
 
-// SETUP
+// APP SETUP
 // ---------------------------------------------
 const app = express();
 app.set("view engine", "ejs");
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended:true }));
 
+// MONGOOSE DB SETUP
+// ---------------------------------------------
+mongoose.connect('mongodb://localhost:27017/KD_Blog_Site');
+
+const blogPostSchema = new mongoose.Schema(
+    {
+        _id: {
+            type: Number,
+            required: [true, 'Set Automatic timestamp as ID']
+        },
+        postHeadline: {
+            type: String,
+            required: [true, 'A Post Headline is needed']
+        },
+        postBody: {
+            type: String,
+            required: [true, 'A Post needs a text!']
+        },
+        postExcerpt: {
+            type: String,
+            required: [true, 'Post Excerpt needed']
+        },
+        postLink: {
+            type: String,
+            required: [true, 'Post Link is needed']
+        }
+    }
+);
+
+const blogSiteSchema = new mongoose.Schema(
+    {
+        siteHeadline: {
+            type: String,
+            required: [true, 'The Page needs a headline']
+        },
+        siteText: String,
+        siteLink: {
+            type: String,
+            required: [true, 'The Site Route is needed']
+        }
+    }
+)
+
+const Site = mongoose.model('Site', blogSiteSchema);
+const Post = mongoose.model('Post', blogPostSchema);
+
 // GLOBAL VARIABLES
 // ---------------------------------------------
-let ejsObjects = {
+let ejsSiteObjects = {
     siteTitle: '',
     siteText: '',
     posts: []
 };
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
+let ejsPostObject = {
+    siteTitle: '',
+    siteText: ''
+}
 
 //ROUTES
 // ---------------------------------------------
-app.get("/", (req, res) => {
-    ejsObjects.siteTitle = 'Home';
-    ejsObjects.siteText = homeStartingContent;
-    res.render("site", ejsObjects);
-    dynamicRoutes();
-});
 
-app.get("/about", (req, res) => {
-    ejsObjects.siteTitle = 'About';
-    ejsObjects.siteText = aboutContent;
-    res.render("site", ejsObjects);
+const siteConstructor = () => {
+            Site.find((err, sites) => {
+            if (err) {
+                console.log(err);
+            } else {
+                sites.forEach((site) => {
+                    app.get(site.siteLink, (req, res) => {
+                        ejsSiteObjects.siteTitle = site.siteHeadline;
+                        ejsSiteObjects.siteText = site.siteText;
+                        if (site.siteHeadline === 'Home') {
+                            let postArray = [];
+                            Post.find((err, posts) => {
+                                if (err) {
+                                    console.log(err);
+                                } else {
+                                    posts.forEach((post) => {
+                                        postArray.push(post);
+                                    });
+                                    ejsSiteObjects.posts = postArray;
+                                }
+                            });
+                            
+                            console.log(ejsSiteObjects.posts);
+                            ejsSiteObjects.posts.forEach((post) => {
+                                app.get(post.postLink, (request, response) => {
+                                    ejsPostObject.siteTitle = post.postHeadline;
+                                    ejsPostObject.siteText = post.postBody;
+                                    response.render("post", ejsPostObject);
+                                });
+                            });
+                        } else {
+                            //ejsSiteObjects.posts = [];
+                            console.log('Was machst Du hier?');
+                        }
+                        res.render("site", ejsSiteObjects);
+                    });
+                });
+            }
+        })
+};
 
-});
-
-app.get("/contact", (req, res) => {
-    ejsObjects.siteTitle = 'Contact';
-    ejsObjects.siteText = contactContent;
-    res.render("site", ejsObjects);
-
-});
-
-app.get("/compose", (req, res) => {
-    ejsObjects.siteTitle = 'Compose';
-    ejsObjects.siteText = '';
-    res.render("site", ejsObjects);
-});
+siteConstructor();
 
 
 // POSTS
@@ -58,14 +124,29 @@ app.post("/", (req, res) => {
     let postHeadline = req.body.postHeadline;
     let postText = req.body.postText;
     let postLink = postHeadline.replace(/[^0-9a-z\ ]/gi, '').toLocaleLowerCase();
+    postLink = '/posts/' + postLink.replace(/ /g,'-');
     let postExcerpt = postText.slice(0, 100);
-    let postObject = {
-        postHeadline: postHeadline,
-        postText: postText,
-        postExcerpt: postExcerpt,
-        postLink: '/posts/' + postLink.replace(/ /g,'-')
-    };
-    ejsObjects.posts.push(postObject);
+
+    const post = new Post(
+        {
+            _id: Date.now(),
+            postHeadline: postHeadline,
+            postBody : postText,
+            postExcerpt: postExcerpt,
+            postLink: postLink
+        }
+    )
+
+    Post.insertMany([post], (err, posts) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('Post Saved Successfully!');
+        }
+    });
+
+    //ejsSiteObjects.posts.push(post);
+    siteConstructor();
     res.redirect("/");
 });
 
@@ -76,20 +157,3 @@ app.listen(process.env.PORT || 3000, () => {
 });
 
 
-
-// GLOBAL FUNCTIONS
-// ---------------------------------------------
-// ---------------------------------------------
-
-
-// Dynamic Routes
-// ---------------------------------------------
-let dynamicRoutes = () => {
-        ejsObjects.posts.forEach((post) => {
-            app.get(post.postLink, (req, res) => {
-                ejsObjects.siteTitle = post.postHeadline;
-                ejsObjects.siteText = post.postText;
-                res.render("post", ejsObjects);
-            });
-        });
-};
